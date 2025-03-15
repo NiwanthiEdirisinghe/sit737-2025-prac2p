a984df6e633739fde589e047bdf7b15cd22d54ab
Niwanthi Edirisinghe <niwanthi.e@wemakeplatforms.io>
2025-03-16 01:15:45 +1100

    Add files

---
+++ server.js
@@ -0,0 +1,15 @@
+var express = require('express');
+var path = require("path");
+
+var app = express();
+const PORT = 3000;
+
+app.use(express.static(__dirname));
+
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, 'index.html'));
+});
+
+app.listen(PORT, () => {
+    console.log(`Server is running at http://localhost:${PORT}`);
+});
